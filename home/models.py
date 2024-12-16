7132c1ccc40497da32631aff06aa8decdf8dcfa8
App Generator <51070104+app-generator@users.noreply.github.com>
2024-12-16 13:45:06 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,56 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Invoice(models.Model):
+
+    #__Invoice_FIELDS__
+    reference = models.CharField(max_length=255, null=True, blank=True)
+    due-date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+
+    #__Invoice_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Invoice")
+        verbose_name_plural = _("Invoice")
+
+
+class Invoiceitem(models.Model):
+
+    #__Invoiceitem_FIELDS__
+    invoiceid = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Invoiceitem_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Invoiceitem")
+        verbose_name_plural = _("Invoiceitem")
+
+
+
+#__MODELS__END
